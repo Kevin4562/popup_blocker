be9dbcb4149420e9bddd5a92e07e4d06fb1bd628
Kevin4562 <37491921+Kevin4562@users.noreply.github.com>
2023-03-07 20:56:34 -0600

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,2 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-$('a').on('click.open', function(e) { e.preventDefault(); window.open('http://disney.com') });
+window.open = function (url, windowName, windowFeatures) {
+   alert("Not opening: " + url);
+}
