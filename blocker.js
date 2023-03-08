39a87096a8bc787aab46bfe8a137565e5fe72b9b
Kevin4562 <37491921+Kevin4562@users.noreply.github.com>
2023-03-07 21:21:52 -0600

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,4 +1,13 @@
-alert("Loaded");
-window.open = function (url, windowName, windowFeatures) {
-   alert("Not opening: " + url);
-}
+setTimeout(function () {
+   alert("loaded");
+   window.open = function (url, windowName, windowFeatures) {
+      alert("Not opening: " + url);
+   }
+
+   Array.from(document.getElementsByTagName("a")).forEach(function(el){
+     el.addEventListener("click",function(e){
+       e.preventDefault();
+   alert(`${el.href} -  ${el.target}`);
+     });
+   });
+}, 500);
