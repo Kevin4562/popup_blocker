6f3bb84e70543f94628ba581de51c66ac46e472a
Kevin4562 <37491921+Kevin4562@users.noreply.github.com>
2023-03-07 21:40:25 -0600

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,5 +1,5 @@
 setTimeout(function () {
-   alert("loaded V4");
+   alert("loaded V5");
    window.location="javascript:window.open=function(){alert('Blocked')};void(0);"
 
    Array.from(document.getElementsByTagName("a")).forEach(function(el){
