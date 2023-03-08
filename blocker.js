b66fd53ee13cafda80328443ef0a9f5d0f15b3be
Kevin4562 <37491921+Kevin4562@users.noreply.github.com>
2023-03-07 21:37:38 -0600

    Update blocker.js

--- blocker.js
+++ blocker.js
@@ -1,10 +1,9 @@
 setTimeout(function () {
-   alert("loaded V2");
-   window.open = function (url, windowName, windowFeatures) {
-      alert("Not opening: " + url);
-   }
+   alert("loaded V3");
+   window.location="javascript:window.open=alert('Blocked');void(0);"
 
    Array.from(document.getElementsByTagName("a")).forEach(function(el){
+      el.target
       el.addEventListener("click",function(e){
          e.preventDefault();
          alert(`${el.href} -  ${el.target}`);
